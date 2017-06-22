1afc289a8954fd191fb6aef5a25b74c659680f15
Dany Sluijk <dany@atlasdev.nl>
2017-06-22 06:31:27 +0200

    Fix basedao 6x

--- src/main/java/nl/atlasdev/IPASS/dao/BaseDAO.java
+++ src/main/java/nl/atlasdev/IPASS/dao/BaseDAO.java
@@ -10,15 +10,16 @@ import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class BaseDAO {
-	private static DataSource connectionPool;
+	private static Connection conn;
 
-	public BaseDAO() {
-		if (BaseDAO.connectionPool != null) return;
+	protected final Connection getConnection() {
+		if(BaseDAO.conn != null) return BaseDAO.conn;
 		try {
 			final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
 			if (DATABASE_URL_PROP != null) {
 				URI dbUri = new URI(DATABASE_URL_PROP);
 				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+				@SuppressWarnings("resource")
 				BasicDataSource pool = new BasicDataSource();
 				if (dbUri.getUserInfo() != null) {
 					pool.setUsername(dbUri.getUserInfo().split(":")[0]);
@@ -27,22 +28,21 @@ public class BaseDAO {
 				pool.setDriverClassName("org.postgresql.Driver");
 				pool.setUrl(dbUrl);
 				pool.setInitialSize(1);
-				connectionPool = pool;
+				BaseDAO.conn = pool.getConnection();
 			} else {
 				InitialContext ic = new InitialContext();
-				connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+				BaseDAO.conn = ((DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS")).getConnection();
+				return BaseDAO.conn;
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
-
-	protected final Connection getConnection() throws SQLException {
-		if (BaseDAO.connectionPool != null) return BaseDAO.connectionPool.getConnection();
 		try {
-			return connectionPool.getConnection();
+			DataSource ds = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/PostgresDS");
+			BaseDAO.conn = ds.getConnection();
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
+		return BaseDAO.conn;
 	}
 }
